a71ef645accec010be028ebc177bf8bf1067dd7b
Edward Bierens <143363343+edwardsqrt-1@users.noreply.github.com>
2025-04-18 22:32:45 -0500

    (purely debugging) Can extract and print data from table
    I will still need to format this data properly, however I wanted to share that progress has been made.

--- include/sql.h
+++ include/sql.h
@@ -4,10 +4,31 @@
 sqlite3* db_con;
 sqlite3_stmt* db_statement;
 
+// Forward declaration of displaying the output;
+void DispOutput();
+
 // Global Variable for Filename
 static char filename[512];
 
 int runSQL(const char* command) {
-	char* error = "error\n";
-	return sqlite3_exec(db_con, command, NULL, NULL, &error);
+	int res, i;
+	char res_str[500];
+
+	res = sqlite3_prepare_v2(db_con, command, -1, &db_statement, NULL);
+
+	res = sqlite3_step(db_statement);
+
+	while (res == SQLITE_ROW) {
+		
+		i = 0;
+		while (sqlite3_column_text(db_statement, i) != NULL) {
+			printf("%s\n", sqlite3_column_text(db_statement, i));
+			i++;
+		}
+		res = sqlite3_step(db_statement);
+	}
+
+	sqlite3_finalize(db_statement);
+
+	return 0;
 }
