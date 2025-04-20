9127f7e6796a98f8226df932c509a808d08ac3e8
Edward Bierens <143363343+edwardsqrt-1@users.noreply.github.com>
2025-04-19 23:05:39 -0500

    Ensure tables are formatted correctly
    While data was extracted in the past, this organizes it into a table format. Still looking how to get the columns to show.

---
+++ include/dbtable.h
@@ -0,0 +1,112 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+// Set the output (will need a lot of memory, that's fun)
+// Can't make this a local variable since otherwise it will fall out of scope
+// And I don't desire to use malloc for this one
+char output[32768];
+
+struct DBTable {
+
+    unsigned int rows;
+    unsigned int columns;
+    char*** data;
+
+};
+
+// Constructor for a table
+struct DBTable InitTable(unsigned int r, unsigned int c) {
+
+    // Declare new table
+    struct DBTable new_table;
+
+    // Set the rows and columns
+    new_table.rows = r;
+    new_table.columns = c;
+
+    // Allocate array of string arrays as rows
+    new_table.data = (char***) malloc(sizeof(char**) * r);
+
+    // Allocate string pointers to each column
+    for (int row = 0; row < r; row++) new_table.data[row] = (char**) malloc(sizeof(char*) * c);
+
+    // Return the set up table
+    return new_table;
+
+}
+
+// Function to print a table
+char* PrintTable(const struct DBTable *table) {
+
+    // Check for a table with no columns or rows
+    if (table->columns == 0 && table->rows == 0) return "<no data given>\n";
+
+    // Delimiters
+    char* t_start = "| ";
+    char* t_delim = " | ";
+    char* t_cease = " |\n";
+    char* iter = 0;
+
+    // Get maximum widths of each piece of data in the table (exceptions made for comments, which have own widths)
+    int dataWidth = 0, commentWidth = 10, tableLength;
+    for (int r = 0; r < table->rows; r++) {
+        for (int c = 0; c < table->columns - 1; c++){
+            if (strlen(table->data[r][c]) > dataWidth) dataWidth = strlen(table->data[r][c]);
+        } 
+        if (strlen(table->data[r][table->columns - 1]) > commentWidth) commentWidth = strlen(table->data[r][table->columns - 1]);
+    }
+
+    // Calculate Table Length
+    tableLength = (table->columns - 1) * (dataWidth + 3) + commentWidth + 4;
+
+    // Print top delimiter
+    for (int i = 0; i < tableLength; i++) strcat(output, "-");
+    strcat(output, "\n");
+
+    // Print each row
+    for (int r = 0; r < table->rows; r++) {
+
+        // Use the start code
+        strcat(output, t_start);
+
+        // Append each column and the delimiter
+        for (int c = 0; c < table->columns; c++) {
+
+            // Add column data, filler space if applicable, and delimiter
+            strcat(output, table->data[r][c]);
+            if (c != table->columns - 1) {
+                for (int e = strlen(table->data[r][c]); e < dataWidth; e++) strcat(output, " ");
+                strcat(output, t_delim);
+            } else for (int e = strlen(table->data[r][c]); e < commentWidth; e++) strcat(output, " ");
+
+        }
+
+        // Use the end code
+        strcat(output, t_cease);
+
+        // If it is the first line, print another delimiter
+        /*if (r == 0) {
+            for (int i = 0; i < tableLength; i++) strcat(output, "-");
+            strcat(output, "\n");
+        }*/
+    }
+
+    // Print another delimiter to end the table
+    for (int i = 0; i < tableLength; i++) strcat(output, "-");
+    strcat(output, "\n");
+
+    // Return the output
+    return output;
+    
+}
+
+
+// Function to delete a table
+void FreeTable(struct DBTable *table) {
+
+    for (int row = 0; row < table->rows; row++) free(table->data[row]);
+    free(table->data);
+ 
+}
+
--- include/sql.h
+++ include/sql.h
@@ -1,4 +1,8 @@
 #include "sqlite3.h"
+#include "dbtable.h"
+
+#define MAX_ROWS 1024
+#define MAX_COLS 100
 
 // Connection helpers
 sqlite3* db_con;
@@ -11,24 +15,41 @@ void DispOutput();
 static char filename[512];
 
 int runSQL(const char* command) {
-	int res, i;
+	int res, c, r;
 	char res_str[500];
 
 	res = sqlite3_prepare_v2(db_con, command, -1, &db_statement, NULL);
 
 	res = sqlite3_step(db_statement);
 
+	struct DBTable result_table;
+
+	result_table.data = (char***) malloc(sizeof(char**) * MAX_ROWS);
+	for (r = 0; r < MAX_ROWS; r++) result_table.data[r] = (char**) malloc(sizeof(char*) * MAX_COLS);
+	r = 0;
+
 	while (res == SQLITE_ROW) {
-		
-		i = 0;
-		while (sqlite3_column_text(db_statement, i) != NULL) {
-			printf("%s\n", sqlite3_column_text(db_statement, i));
-			i++;
+		c = 0;
+		while (sqlite3_column_text(db_statement, c) != NULL) {
+			result_table.data[r][c] = (char*) malloc(strlen(sqlite3_column_text(db_statement, c)) + 1);
+			strcpy(result_table.data[r][c], sqlite3_column_text(db_statement, c));
+			c++;
 		}
 		res = sqlite3_step(db_statement);
+		r++;
 	}
 
+	result_table.rows = r;
+	result_table.columns = c;
+
+	printf("%s", PrintTable(&result_table));
+
+
 	sqlite3_finalize(db_statement);
+
+	for (r = 0; r < result_table.rows; r++) for (c = 0; c < result_table.columns; c++) free(result_table.data[r][c]);	
+	for (r = 0; r < MAX_ROWS; r++) free(result_table.data[r]);
+	free(result_table.data);
 
 	return 0;
 }
